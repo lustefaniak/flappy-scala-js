3d7f707fef14ed128b33457af2c4057a28cd0fe3
Lukas Stefaniak <lustefaniak@gmail.com>
2015-03-10 00:39:15 +0100

    Initial template

---
+++ src/main/scala/example/ScalaJSExample.scala
@@ -0,0 +1,37 @@
+package example
+
+import scala.scalajs.js.annotation.JSExport
+import org.scalajs.dom
+import org.scalajs.dom._
+
+@JSExport
+object ScalaJSExample {
+  @JSExport
+  def main(canvas: html.Canvas): Unit = {
+    resizeCanvas(canvas)
+    window.onresize = (e: UIEvent) => {
+      resizeCanvas(canvas)
+    }
+
+    val ctx = canvas.getContext("2d")
+      .asInstanceOf[dom.CanvasRenderingContext2D]
+
+    def clear() = {
+      ctx.fillStyle = "black"
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+    }
+
+    def run = {
+      clear()
+    }
+
+    dom.setInterval(() => run, 16)
+  }
+
+
+  def resizeCanvas(canvas: html.Canvas): Unit = {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+  }
+
+}
