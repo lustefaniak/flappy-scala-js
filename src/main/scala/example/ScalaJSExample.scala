89569369f52cacc8910c0a66c4a2dc30f8521c3e
Lukas Stefaniak <lustefaniak@gmail.com>
2015-03-10 23:53:17 +0100

    Add game state so it is possible to restart without reload

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -13,37 +13,40 @@ import scala.util.Random
 object ScalaJSExample {
   @JSExport
   def main(canvas: html.Canvas): Unit = {
+
+    trait GameState {
+      var alive = true
+      val frameTime: Double = 16.0 / 1000.0
+      val playerSize: Double = 50
+      var player = Vect(100, canvas.height / 2 - playerSize / 2)
+      val acceleration = Vect(0, 0.3)
+      var speed = Vect(0, 0)
+      val enemyEvery: Double = canvas.width / 3
+      var enemySpeed: Double = 5
+      var frame: Long = 0
+      var score: Long = 0
+      var enemies: Seq[(Double, Double)] = (for (i <- 2 to 20) yield (i * enemyEvery, genEnemy(canvas.height))).toSeq
+    }
+
+    var gameState = new GameState {}
+
     resizeCanvas(canvas)
     window.onresize = (e: UIEvent) => {
       resizeCanvas(canvas)
+      gameState = new GameState {}
     }
 
-    val keys = mutable.Set[Int]()
-
+    var jump = false
     window.onkeydown = (ke: KeyboardEvent) => {
-      keys.add(ke.keyCode)
+      if (ke.keyCode == ext.KeyCode.space) jump = true
     }
-
     window.onkeyup = (ke: KeyboardEvent) => {
-      keys.remove(ke.keyCode)
+      if (ke.keyCode == ext.KeyCode.space) jump = false
+      if (ke.keyCode == ext.KeyCode.r) gameState = new GameState {}
     }
 
     val ctx = canvas.getContext("2d")
       .asInstanceOf[dom.CanvasRenderingContext2D]
-
-    var alive = true
-    val frameTime: Double = 16.0 / 1000.0
-    val playerSize = 50
-    var player = Vect(100, canvas.height / 2 - playerSize / 2)
-    val acceleration = Vect(0, 0.3)
-    var speed = Vect(0, 0)
-    val enemyEvery: Double = canvas.width / 3
-    var enemySpeed: Double = 5
-    var frame: Long = 0
-
-    var score: Long = 0
-
-    var enemies: Seq[(Double, Double)] = (for (i <- 2 to 20) yield (i * enemyEvery, genEnemy(canvas.height))).toSeq
 
     def clear() = {
       ctx.fillStyle = "black"
@@ -51,60 +54,61 @@ object ScalaJSExample {
     }
 
     def update(): Unit = {
-      frame += 1
+      gameState.frame += 1
 
-      speed = speed + acceleration
-      player = player + speed
+      gameState.speed = gameState.speed + gameState.acceleration
+      gameState.player = gameState.player + gameState.speed
 
-      if (keys.contains(ext.KeyCode.space)) {
-        speed = Vect(0, -7)
+      if (jump) {
+        gameState.speed = Vect(0, -7)
       }
 
-      val maxX = enemies.map(_._1).max
-      enemies = enemies.map {
-        case (x, y) => if (x > -playerSize)
-          (x - enemySpeed, y)
+      val maxX = gameState.enemies.map(_._1).max
+      gameState.enemies = gameState.enemies.map {
+        case (x, y) => if (x > -gameState.playerSize)
+          (x - gameState.enemySpeed, y)
         else {
-          score += 1
-          enemySpeed = 5 * Math.pow(1.03, score)
-          (maxX + enemyEvery, genEnemy(canvas.height))
+          gameState.score += 1
+          gameState.enemySpeed = 5 * Math.pow(1.03, gameState.score)
+          (maxX + gameState.enemyEvery, genEnemy(canvas.height))
         }
       }
 
-      if (player.y + playerSize > canvas.height || player.y < 0) alive = false
+      if (gameState.player.y + gameState.playerSize > canvas.height || gameState.player.y < 0) gameState.alive = false
 
-      enemies.foreach {
+      gameState.enemies.foreach {
         case (x, y) =>
-          if (player.x > x && player.x < x + playerSize || player.x + playerSize > x && player.x + playerSize < x + playerSize) {
-            if (player.y > y + playerSize * 2 || player.y < y - playerSize * 2)
-              alive = false
+          if (gameState.player.x > x && gameState.player.x < x + gameState.playerSize || gameState.player.x + gameState.playerSize > x && gameState.player.x + gameState.playerSize < x + gameState.playerSize) {
+            if (gameState.player.y > y + gameState.playerSize * 2 || gameState.player.y < y - gameState.playerSize * 2)
+              gameState.alive = false
           }
       }
     }
 
 
     def run = {
-      if (alive)
+      if (gameState.alive)
         update()
+
       clear()
 
-      ctx.fillStyle = if (alive) "white" else "gray"
-      ctx.fillRect(player.x, player.y, playerSize, playerSize)
+      ctx.fillStyle = if (gameState.alive) "white" else "gray"
+      ctx.fillRect(gameState.player.x, gameState.player.y, gameState.playerSize, gameState.playerSize)
 
       ctx.fillStyle = "red"
-      enemies.foreach {
+      gameState.enemies.foreach {
         case (x, y) =>
-          ctx.fillRect(x, y - 2 * playerSize, playerSize, -canvas.height)
-          ctx.fillRect(x, y + 2 * playerSize, playerSize, canvas.height)
+          ctx.fillRect(x, y - 2 * gameState.playerSize, gameState.playerSize, -canvas.height)
+          ctx.fillRect(x, y + 2 * gameState.playerSize, gameState.playerSize, canvas.height)
       }
 
       ctx.font = "20px Helvetica"
       ctx.fillStyle = "green"
-      ctx.fillText("To jump press <space>, to restart reload page.", 10, canvas.height - 10)
+      ctx.fillText("To jump press <space>, to restart <r> or reload page.", 10, canvas.height - 10)
 
       ctx.font = "40px Helvetica"
       ctx.fillStyle = "green"
-      ctx.fillText(score.toString, 10, 40)
+      ctx.fillText(gameState.score.toString, 10, 40)
 
 
     }
