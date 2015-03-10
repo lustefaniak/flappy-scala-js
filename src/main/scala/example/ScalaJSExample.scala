a614972409aa29a73be83c2e61904a5a9c26e939
Lukas Stefaniak <lustefaniak@gmail.com>
2015-03-10 01:39:44 +0100

    Simple phisics movement and make him die

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -16,11 +16,11 @@ object ScalaJSExample {
 
     val keys = mutable.Set[Int]()
 
-    window.onkeydown = (ke:KeyboardEvent)=>{
+    window.onkeydown = (ke: KeyboardEvent) => {
       keys.add(ke.keyCode)
     }
 
-    window.onkeyup = (ke:KeyboardEvent)=>{
+    window.onkeyup = (ke: KeyboardEvent) => {
       keys.remove(ke.keyCode)
     }
 
@@ -28,11 +28,12 @@ object ScalaJSExample {
       .asInstanceOf[dom.CanvasRenderingContext2D]
 
 
-    val frameTime:Double = 16.0/1000.0
+    var alive = true
+    val frameTime: Double = 16.0 / 1000.0
     val playerSize = 50
-    var player = Vect(100, 0)
-    val acceleration = Vect(0,50 0)
-    var speed = Vect(0,0)
+    var player = Vect(100, canvas.height / 2 - playerSize / 2)
+    val acceleration = Vect(0, 0.3)
+    var speed = Vect(0, 0)
 
     def clear() = {
       ctx.fillStyle = "black"
@@ -40,17 +41,25 @@ object ScalaJSExample {
     }
 
     def update(): Unit = {
-      speed = speed + acceleration * frameTime
-      player = player + speed * frameTime
-      
+      speed = speed + acceleration
+      player = player + speed
+
+      if (keys.contains(ext.KeyCode.space)) {
+        speed = Vect(0, -5)
+      }
+
+
+      if (player.y + playerSize > canvas.height || player.y < 0) alive = false
+
     }
 
 
     def run = {
-      update()
+      if (alive)
+        update()
       clear()
 
-      ctx.fillStyle = "white"
+      ctx.fillStyle = if (alive) "white" else "gray"
       ctx.fillRect(player.x, player.y, playerSize, playerSize)
 
     }
